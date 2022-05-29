c49c4591823107adf7c97a2d9293cb5d55d3ddb9
Dhrgon <cpbitsch@gmail.com>
2022-05-28 21:07:14 -0600

    base code upload

---
+++ scripts/pokemon_generator.js
@@ -0,0 +1,30 @@
+input = ""
+
+function output(element) {
+    let pokemon = document.createElement("article")
+    let pokemonName = document.createElement("h3")
+    pokemonName.innerHTML = element.name
+    pokemonName.innerHTML = "Name: " + pokemonName.innerHTML.charAt(0).toUpperCase() + pokemonName.innerHTML.slice(1)
+    let pokemonId = document.createElement("h4")
+    pokemonId.innerHTML = "ID: " + element.id
+    let pokemonSprite = document.createElement("img")
+    pokemonSprite.setAttribute("src", element.sprites.front_default)
+    pokemonSprite.setAttribute("alt", element.name)
+    document.getElementById("pokemon").appendChild(pokemon)
+    pokemon.appendChild(pokemonName)
+    pokemon.appendChild(pokemonId)
+    pokemon.appendChild(pokemonSprite)
+    document.getElementById("pokemon").appendChild(pokemon)
+}
+
+async function get_pokemon(input) {
+    input = document.getElementById("input_pokemon").value
+    let url = `https://pokeapi.co/api/v2/pokemon/${input}`
+    let response = await fetch(url)
+    pokemon = await response.json()
+    output(pokemon)
+}
+
+window.onload = function(){
+    document.getElementById("button").addEventListener("click", get_pokemon)
+}
